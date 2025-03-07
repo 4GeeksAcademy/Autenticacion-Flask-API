1615b362536513e29e7ac3328b4cc01ad43f82e3
Alejandro <alejandrolujan1999@outlook.es>
2025-03-07 02:00:41 +0000

    Version 0.1

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,26 @@
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer bg-dark py-3 text-center text-light mt-5 shadow-sm">
+	  <div className="container">
 		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
+		  Check the{" "}
+		  <a
+			target="_blank"
+			rel="noopener noreferrer"
+			href="https://4geeks.com/docs/start/react-flask-template"
+			className="text-light"
+		  >
+			template documentation
+		  </a>{" "}
+		  <i className="fa-solid fa-file"></i> for help.
 		</p>
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+		  Made with{" "}
+		  <i className="fa fa-heart text-danger" /> by{" "}
+		  <a href="http://www.4geeksacademy.com" className="text-light">
+			4Geeks Academy
+		  </a>
 		</p>
+	  </div>
 	</footer>
-);
+  );
+  
